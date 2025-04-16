7b98811f526ffd22bad48ee422e83cef32b1fe8f
viveknarayana <vivekpersonal400@gmail.com>
2025-04-16 00:55:07 -0700

    Groq coversation flow - add tool exposures for docker

--- terminal_agent/agent.py
+++ terminal_agent/agent.py
@@ -1,13 +1,15 @@
 import os
 from dotenv import load_dotenv
-from anthropic import Anthropic
+from groq import Groq
 from typing import List, Dict, Any
 
 load_dotenv()
 
 class AIAgent:
     def __init__(self):
-        self.anthropic = Anthropic()
+        self.groq = Groq()
+        self.model = 'llama-3.3-70b-versatile'
+        self.tools = []
         #print("HELLO ", os.environ.get("ANTHROPIC_API_KEY"))
         # It should have its own conversation history in case I want to pass back
         self.conversation_history = []
@@ -24,17 +26,20 @@ class AIAgent:
         
         messages = [{"role": "system", "content": self.system_prompt}] + self.conversation_history
 
-        response = self.anthropic.messages.create(
-            model="claude-3-7-sonnet-20250219",
-            max_tokens=2000,
-            messages=messages
+        response = self.groq.chat.completions.create(
+            model=self.model, 
+            messages=messages, 
+            stream=False,
+            tools=self.tools, # TO BE ADDED
+            tool_choice="auto", # Lets GROQ decide when to use tool - implement routing later
+            max_completion_tokens=4096 # Maximum number of tokens to allow in our response
         )
-        
-        content = response.content[0].text
-        self.add_message("assistant", content)
+
+        response_message = response.choices[0].message.content
+        self.add_message("assistant", response_message)
         
         
         return {
-            "response_text": content
+            "response_text": response_message
         }
     
